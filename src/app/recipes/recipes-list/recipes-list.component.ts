eb77e53be5595d25134bf5cbb4f6afd2368eb53f
ahmed.razic <ahmed.razic@gmail.com>
2022-10-30 10:46:00 +0100

    project structure

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simple recipe',
+      'https://therecipecritic.com/wp-content/uploads/2021/03/bakedrigatoni-368x463.jpg'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
